3abab64fbbfca169df69271ce4408ebb3e817d05
matyasgubis <gubismatyas@gmail.com>
2023-10-15 21:52:25 +0200

    Optimising

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import time
 import turtle
 
+from ball import Ball
 from paddle import Paddle
 
 screen = turtle.Screen()
@@ -11,17 +12,52 @@ screen.setup(width=800, height=600)
 move_up = False
 
 screen.tracer(0)
-player_1 = Paddle((-350, 0))
-player_2 = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+r_paddle = Paddle((350, 0))
+ball = Ball()
 screen.update()
 screen.listen()
-screen.onkey(player_2.move_up, "Up")
-screen.onkey(player_2.move_down, "Down")
-screen.onkey(player_1.move_up, "w")
-screen.onkey(player_1.move_down, "s")
+screen.onkeypress(r_paddle.turn_on_move_up, "Up")
+screen.onkeypress(r_paddle.turn_on_move_down, "Down")
+screen.onkeypress(l_paddle.turn_on_move_up, "w")
+screen.onkeypress(l_paddle.turn_on_move_down, "s")
+screen.onkeyrelease(r_paddle.stop, "Up")
+screen.onkeyrelease(r_paddle.stop, "Down")
+screen.onkeyrelease(l_paddle.stop, "w")
+screen.onkeyrelease(l_paddle.stop, "s")
 
 game_over = False
 while not game_over:
+    time.sleep(0.01)
+    ball.move()
     screen.update()
+    ball.bounce_timer -= .01
+
+    if l_paddle.should_move_up:
+        l_paddle.move_up()
+
+    if r_paddle.should_move_up:
+        r_paddle.move_up()
+
+    if l_paddle.should_move_down:
+        l_paddle.move_down()
+
+    if r_paddle.should_move_down:
+        r_paddle.move_down()
+
+    # Detect collision with right paddle
+    if ball.distance(r_paddle) < 50 and 330 < ball.xcor() < 360 \
+            or ball.distance(l_paddle) < 50 and -330 > ball.xcor() > -360:
+        ball.bounce_x()
+
+    if ball.xcor() > 380:
+        l_paddle.score()
+        ball.restart()
+        print(f"l_paddle score: {l_paddle.points} r_paddle score: {r_paddle.points}")
+
+    if ball.xcor() < -380:
+        r_paddle.score()
+        ball.restart()
+        print(f"l_paddle score: {l_paddle.points} r_paddle score: {r_paddle.points}")
 
 screen.exitonclick()
--- paddle.py
+++ paddle.py
@@ -10,9 +10,29 @@ class Paddle(Turtle):
         self.color("white")
         self.penup()
         self.goto(starting_position)
+        self.points = 0
+        self.should_move_up = False
+        self.should_move_down = False
+
+    def turn_on_move_up(self):
+        self.should_move_up = True
+
+    def turn_on_move_down(self):
+        self.should_move_down = True
+
+    def stop(self):
+        self.should_move_up = False
+        self.should_move_down = False
 
     def move_up(self):
-        self.goto(self.xcor(), self.ycor() + 20)
+        if self.ycor() < 250:
+            self.goto(self.xcor(), self.ycor() + 5)
 
     def move_down(self):
-        self.goto(self.xcor(), self.ycor() - 20)
+        if self.ycor() > -240:
+            self.goto(self.xcor(), self.ycor() - 5)
+
+    def score(self):
+        self.points += 1
+
+
